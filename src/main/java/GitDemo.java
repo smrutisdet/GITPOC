fb43e409154a7ff36225fe597d2afae09e265d0a
smohapatra <smmohapatra011@gmail.com>
2022-04-11 14:55:55 +0530

    commit 2

--- src/main/java/GitDemo.java
+++ src/main/java/GitDemo.java
@@ -1,5 +1,6 @@
 public class GitDemo {
     public static void main(String[] args) {
         System.out.println("GitDemo Class in main/java");
+        System.out.println("This line is added for second commit");
     }
 }
